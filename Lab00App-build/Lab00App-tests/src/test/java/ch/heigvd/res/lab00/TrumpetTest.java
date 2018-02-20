606c1e29f445b215f9cbc12517cab0af90f69132
Olivier Liechti <olivier.liechti@wasabi-tech.com>
2018-02-20 19:33:58 +0100

    Add validation code

--- Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/TrumpetTest.java
+++ Lab00App-build/Lab00App-tests/src/test/java/ch/heigvd/res/lab00/TrumpetTest.java
@@ -15,7 +15,6 @@ import org.junit.Test;
  */
 public class TrumpetTest {
 
-  /*
   @Test
   public void thereShouldBeAnIInstrumentInterfaceAndATrumpetClass() {
     IInstrument trumpet = new Trumpet();
@@ -51,6 +50,5 @@ public class TrumpetTest {
     String color = trumpet.getColor();
     Assert.assertEquals("golden", color);
   }
-  */
  
 }
